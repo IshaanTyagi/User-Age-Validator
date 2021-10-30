f7f1285db3f3ba8290c9ccb4ae97f1f7e60a9470
Ishaan Tyagi <82907648+IshaanTyagi@users.noreply.github.com>
2021-10-30 21:00:05 +0530

    Changed the format to display name and age.

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Card from "../UI/Card";
+import Style from "./Users.module.css";
 function Users(props) {
   return props.Users.map((user) => {
     return (
-      <Card>
-        {user.name} {user.age}
+      <Card className={Style.result}>
+        {user.name} - {user.age}
       </Card>
     );
   });
